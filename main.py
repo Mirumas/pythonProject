e84a87b510b0da2034473225ee5ed47c247fa29b
iliab <iliabob67@gmail.com>
2023-11-26 20:47:39 +0700

    test

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+async def f_index():
+    return {"message": "Hello"}
+
+@app.get('/tools')
+async def f_index():
+    return {"message": "tool"}
